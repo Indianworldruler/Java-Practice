2fd56c385fe756b17db5b351b0b383b0f349f0c3
ParthSahani <107539251+Indianworldruler@users.noreply.github.com>
2025-07-04 11:06:36 +0530

    Create ForLoopExample.java

---
+++ ForLoopExample.java
@@ -0,0 +1,9 @@
+public class ForLoopExample {
+    public static void main(String[] args) {
+        System.out.println("Numbers from 1 to 5 using for loop:");
+        
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Number: " + i);
+        }
+    }
+}
