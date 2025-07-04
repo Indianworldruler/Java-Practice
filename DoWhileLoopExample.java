1598db269fa611cbb6ca87c584da287eef7d8c04
ParthSahani <107539251+Indianworldruler@users.noreply.github.com>
2025-07-04 11:08:05 +0530

    Create DoWhileLoopExample.java

---
+++ DoWhileLoopExample.java
@@ -0,0 +1,12 @@
+public class DoWhileLoopExample {
+    public static void main(String[] args) {
+        int i = 1;
+
+        System.out.println("Numbers from 1 to 5 using do-while loop:");
+        
+        do {
+            System.out.println("Number: " + i);
+            i++;
+        } while (i <= 5);
+    }
+}
